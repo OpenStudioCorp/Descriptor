99dd45cbc613f0caa6bc96a395f38656fdef18c9
charlie-sans <gamerboy030607@gmail.com>
2024-05-06 02:05:34 +1000

    Add project files.

---
+++ Kernel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace Descriptor
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Welcome();
+        }
+
+        protected override void Run()
+        {
+         
+        }
+    }
+}
