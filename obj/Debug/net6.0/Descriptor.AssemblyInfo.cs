792274def552c0341e85dcc8b0f2128aac11ac89
biotest05 <biotest05@DESKTOP-BIO>
2024-05-06 15:22:34 +0200

    :D

--- obj/Debug/net6.0/Descriptor.AssemblyInfo.cs
+++ obj/Debug/net6.0/Descriptor.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Use VMware Player or Workstation to deploy and debug.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acae30da936c495e2ea33d5465d07dbcc02de306")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6548c2b621be6fd6621b610122640d1c0fae72e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Descriptor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Descriptor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
