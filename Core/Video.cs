fe9f4f5e04157308c7a7733896b04248c9da00e6
WattleFoxxo <88825700+WattleFoxxo@users.noreply.github.com>
2024-05-08 03:50:41 +1000

    Refactor
    - Added `Printf(string)` with ansi colour code parsing
    - Added logging helpers `Logging.Info(string)`, `Logging.warn(string)`,  `Logging.error(string)`
    - Refracted filesystem code
    - Removed Konsole.cs

---
+++ Core/Video.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmos.Core;
+
+//using CSystem.Text.RegularExpressions; // using System.Text.RegularExpressions; Fixed in .net 8
+
+namespace Descriptor.Core
+{
+    internal class Video
+    {
+        public static void Printf(string text)
+        {
+            
+            int index = 0;
+            while (index < text.Length)
+            {
+                if (text[index] == '\u001b' && text[index + 1] == '[')
+                {
+                    int endIndex = text.IndexOf('m', index);
+                    if (endIndex != -1)
+                    {
+                        string code = text.Substring(index + 2, endIndex - index - 2);
+                        
+                        string colourCode = "0";
+                        if (code.Contains(';'))
+                            colourCode = code.Split(';')[1];
+
+                        if (int.TryParse(colourCode, out int colorNum))
+                        {
+                            Console.ForegroundColor = GetConsoleColor(colorNum);
+                        }
+                        index = endIndex + 1;
+                        continue;
+                    }
+                }
+
+                Console.Write(text[index]);
+                index++;
+            }
+        }
+
+        static ConsoleColor GetConsoleColor(int colorCode)
+        {
+            switch (colorCode)
+            {
+                case 30: return ConsoleColor.Black;
+                case 31: return ConsoleColor.Red;
+                case 32: return ConsoleColor.Green;
+                case 33: return ConsoleColor.Yellow;
+                case 34: return ConsoleColor.Blue;
+                case 35: return ConsoleColor.Magenta;
+                case 36: return ConsoleColor.Cyan;
+                case 37: return ConsoleColor.White;
+                default: return ConsoleColor.White;
+            }
+        }
+    }
+
+    internal class Colours8
+    {
+        public static string Black { get; } = "\u001b[0;30m";
+        public static string Red { get; } = "\u001b[0;31m";
+        public static string Green { get; } = "\u001b[0;32m";
+        public static string Yellow { get; } = "\u001b[0;33m";
+        public static string Blue { get; } = "\u001b[0;34m";
+        public static string Purple { get; } = "\u001b[0;35m";
+        public static string Cyan { get; } = "\u001b[0;36m";
+        public static string White { get; } = "\u001b[0;37m";
+    }
+}
--- Kernel.cs
+++ Kernel.cs
@@ -1,40 +1,51 @@
-﻿using Cosmos.System.FileSystem;
-using System;
+﻿using System;
 using Sys = Cosmos.System;
 using System.Threading;
 using System.Reflection.Metadata;
+
+using Descriptor.Core;
+using static Descriptor.Core.Video;
+using System.Runtime.InteropServices;
+
 namespace Descriptor
 {
     public class Kernel : Sys.Kernel
     {
-        public string Version = "1.0.0";
+        public string Version = "0.1.0";
         public static string Path = @"0:\\";
-        public static CosmosVFS fs;
+        //public static CosmosVFS fs;
         protected override void BeforeRun()
         {
-#pragma warning disable CA1416 // Validate platform compatibility
-            Console.SetWindowSize(90, 30);
-#pragma warning restore CA1416 // Validate platform compatibility
-            Console.OutputEncoding = Cosmos.System.ExtendedASCII.CosmosEncodingProvider.Instance.GetEncoding(437);
-            CosmosVFS fs = new Cosmos.System.FileSystem.CosmosVFS();
-            Cosmos.System.FileSystem.VFS.VFSManager.RegisterVFS(fs);
-            Konsol.Kwelcome();
+            Logging.Clear();
+            Logging.Info("Kernel", string.Format("Starting Descriptor kernel (v{0}).", Version));
+
+            Logging.Info("Kernel", "Info log test.");
+            Logging.Warn("Kernel", "Warn log test.");
+            Logging.Error("Kernel", "Error log test.");
+
+            FileSystem.StartFileSystem();
+
+
+            //#pragma warning disable CA1416 // Validate platform compatibility
+            //            Console.SetWindowSize(90, 30);
+            //#pragma warning restore CA1416 // Validate platform compatibility
+            //            Console.OutputEncoding = Sys.ExtendedASCII.CosmosEncodingProvider.Instance.GetEncoding(437);
+            //            CosmosVFS fs = new CosmosVFS();
+            //            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
         }
-    
+
         protected override void Run()
         {
-            
+
             try
             {
-                Konsol.Konsole("$: ", Path);
+                //Konsol.Konsole("$: ", Path);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                Konsol.HandleKernelPanic(e);
+                //Konsol.HandleKernelPanic(e);
             }
-
-
         }
     }
 }
